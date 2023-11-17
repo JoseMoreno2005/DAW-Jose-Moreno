51053857365cbd9848e479dfd6b50e5b5da84874
Jose <morenoortizjose2005@gmail.com>
2023-11-17 10:17:38 +0100

    Ejercicios commit

---
+++ Programación_JM/Programacion_JM.java
@@ -0,0 +1,8 @@
+package programacion_jm;
+public class Programacion_JM {
+    public static void main(String[] args) {
+        System.out.println("Esta asignatura es para aprender a programar en java y es impartida por Lorena");
+
+    }
+    
+}
